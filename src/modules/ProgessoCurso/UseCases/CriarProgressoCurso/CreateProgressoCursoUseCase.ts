4bae679f01888c9ac30f1d1b5ec159cadaefcf60
felixdomingos1 <felixsdomingos93@gmail.com>
2024-09-27 08:03:16 +0100

    atualizadas

--- src/modules/ProgessoCurso/UseCases/CriarProgressoCurso/CreateProgressoCursoUseCase.ts
+++ src/modules/ProgessoCurso/UseCases/CriarProgressoCurso/CreateProgressoCursoUseCase.ts
@@ -1,9 +1,30 @@
+import { AppError } from '../../../../Error/AppError';
+import { ICursoRepository } from '../../../Curso/Repository/ICursoRepository';
+import { IUsuarioRepository } from '../../../Usuario/Repository/IUsuarioRepository';
 import { ICreateProgressoCursoDTO, IProgressoCursoRepository } from '../../Repository/IProgressoCursoRepository';
 
 class CreateProgressoCursoUseCase {
-  constructor(private createProgressoCurso: IProgressoCursoRepository) {}
+  constructor(
+    private createProgressoCurso: IProgressoCursoRepository,
+    private usuarioRepository: IUsuarioRepository,
+    private cursoRepository: ICursoRepository
+
+  ) {}
+
 
   async execute(data: ICreateProgressoCursoDTO) {
+
+    const cursoExist = await this.cursoRepository.findById(data.cursoId)
+    const usuarioExist = await this.usuarioRepository.findById(data.usuarioId)
+
+    if (!usuarioExist) {
+      throw new AppError('Usuario não Existe',404)
+    }
+
+    if (!cursoExist) {
+      throw new AppError('Curso não Existe',404)
+    }
+
     const progressoCurso = await this.createProgressoCurso.create(data);
     return progressoCurso;
   }
--- src/modules/ProgessoCurso/UseCases/CriarProgressoCurso/index.ts
+++ src/modules/ProgessoCurso/UseCases/CriarProgressoCurso/index.ts
@@ -1,9 +1,13 @@
+import { CursoRepository } from "../../../Curso/Repository/implements/CursoRepository"
+import { UsuarioRepository } from "../../../Usuario/Repository/implements/UsuarioRepository"
 import { ProgressoCursoRepository } from "../../Repository/implements/ProgressoCursoRepository"
 import { ProgressoCursoController } from "./CreateProgressoCursoController"
 import { CreateProgressoCursoUseCase } from "./CreateProgressoCursoUseCase"
 
 const  progressoCursoRepository= new ProgressoCursoRepository()
-const createProgressoCursoUseCase = new CreateProgressoCursoUseCase(progressoCursoRepository)
+const cursoRepository = new CursoRepository()
+const usuarioRepository = new UsuarioRepository()
+const createProgressoCursoUseCase = new CreateProgressoCursoUseCase(progressoCursoRepository, usuarioRepository, cursoRepository)
 const createProgressoCurso = new ProgressoCursoController(createProgressoCursoUseCase)
 
 export {createProgressoCurso}
